0fcdbcbeb9bd85e986ad5aa4e7153681dbd9ca34
lenelise <lenelise@gmail.com>
2024-06-10 19:00:13 +0200

    fjerne unødvendige filer

--- Liste.java
+++
@@ -1,8 +0,0 @@
-// import java.util.Iterator;
-
-public interface Liste <E> extends Iterable<E>{
-    public int stoerrelse();
-    public void leggTil(E x);
-    public E hent();
-    public E fjern();
-}
